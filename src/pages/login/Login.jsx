87edd2e50d7d27cf61d6e7e60239aefda2ef40b9
Deki Yangden <12200042.gcit@rub.edu.bt>
2022-06-14 15:55:51 +0600

    update

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,7 @@ import { useState, useContext } from "react"
 import "./login.scss"
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../components/context/AuthContext";
 
 export const Login = () => {
@@ -11,32 +11,33 @@ export const Login = () => {
   const [password, setPassword] = useState("");
 
   const navitage = useNavigate()
-  const {dispatch} = useContext(AuthContext)
+  const { dispatch } = useContext(AuthContext)
 
-  const handleLogin = (e)=>{
+  const handleLogin = (e) => {
     e.preventDefault()
 
     signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-    const user = userCredential.user;
-    dispatch({type:"LOGIN", payload:user})
-    navitage("home");
-  })
-  .catch((error) => {
-    setError(error)
-  });
-}
+      .then((userCredential) => {
+        const user = userCredential.user;
+        dispatch({ type: "LOGIN", payload: user })
+        navitage("home");
+      })
+      .catch((error) => {
+        setError(error)
+      });
+  }
   return (
     <div className="login">
       <form onSubmit={handleLogin}>
-        <input 
-        type="email" 
-        placeholder="email" 
-        onChange={(e)=>setEmail(e.target.value)}/>
-        <input 
-        type="password" 
-        placeholder="password" 
-        onChange={(e)=>setPassword(e.target.value)}/>
+        <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)} />
         <button type="submit">Login</button>
         {error && <span>Wrong email or password!</span>}
       </form>
